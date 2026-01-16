d122749322c91b9d23e9c31efe754b29c03a6b53
saatvik-10 <saatvikmadan529@gmail.com>
2026-01-16 17:43:25 +0530

    redis room id creation

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,6 +1,24 @@
 import { Elysia } from "elysia";
+import { nanoid } from "nanoid";
+import { redis } from "../../../../lib/redis";
 
-const app = new Elysia({ prefix: "/api" }).get("/user", "Hello Nextjs");
+const rooms = new Elysia({ prefix: "/room" }).post("/create", async () => {
+  const roomId = nanoid();
+
+  await redis.hset(`meta_room_id: ${roomId}`, {
+    connected: [],
+    createdAt: Date.now(),
+  });
+
+  await redis.expire(
+    `meta_room_id: ${roomId}`,
+    Number(process.env.ROOM_TTL_SECONDS),
+  );
+
+  return { roomId };
+});
+
+const app = new Elysia({ prefix: "/api" }).use(rooms);
 
 export type App = typeof app;
 
