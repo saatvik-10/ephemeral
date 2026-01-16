2ab26f8c3e2c7b5323c7725f1fc0738a13980b28
saatvik-10 <saatvikmadan529@gmail.com>
2026-01-16 16:25:54 +0530

    creating username function

---
+++ constants/username.ts
@@ -0,0 +1,25 @@
+export const USERNAME = [
+  "lionel_messi",
+  "cristiano_ronaldo",
+  "neymar_jr",
+  "kylian_mbappe",
+  "kevin_de_bruyne",
+  "robert_lewandowski",
+  "luis_suarez",
+  "pedri",
+  "frenkie_de_jong",
+  "david_beckham",
+
+  "john_cena",
+  "the_rock",
+  "roman_reigns",
+  "brock_lesnar",
+  "the_undertaker",
+  "aj_styles",
+  "triple_h",
+  "randy_orton",
+  "seth_rollins",
+  "cm_punk",
+];
+
+export const USER_CHAT_KEY = "user_talk_sync";
