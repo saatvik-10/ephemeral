be4fa89928da1318ba7469a80901ac9c4a629734
saatvik-10 <saatvikmadan529@gmail.com>
2026-01-17 23:32:23 +0530

    fixed hydration err

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${jetbrainsMono.variable} antialiased`}>
+    <html lang="en">
+      <body
+        className={`${jetbrainsMono.variable} antialiased`}
+        suppressHydrationWarning
+      >
         <Providers>{children}</Providers>
       </body>
     </html>
