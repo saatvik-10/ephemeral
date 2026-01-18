363516028605cfd827b2d428a77c407b9e5b1736
saatvik-10 <saatvikmadan529@gmail.com>
2026-01-18 23:27:42 +0530

    updated treaty link

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -5,5 +5,5 @@ import type { App } from "../app/api/[[...slugs]]/route";
 export const client =
   // process is defined on server side and build time
   typeof process !== "undefined"
-    ? treaty<App>("http://localhost:3000").api
+    ? treaty<App>("https://ephemeral-nine.vercel.app/").api
     : treaty<App>("localhost:3000").api;
