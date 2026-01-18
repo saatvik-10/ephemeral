6e4342b467b82d04b13ac9b5613896385b47f99f
saatvik-10 <saatvikmadan529@gmail.com>
2026-01-18 23:38:00 +0530

    cors fixes

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -4,4 +4,11 @@ import type { App } from "../app/api/[[...slugs]]/route";
 // .api to enter /api prefix
 export const client =
   // process is defined on server side and build time
-  treaty<App>("https://ephemeral-nine.vercel.app/").api;
+  typeof process !== "undefined"
+    ? treaty<App>(process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000")
+        .api
+    : treaty<App>(
+        typeof window !== "undefined"
+          ? window.location.origin
+          : "http://localhost:3000",
+      ).api;
